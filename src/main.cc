c3f0045a91e061742400a18e4719214f89ea4035
timmer <timmerov+git@gmail.com>
2023-01-05 14:11:48 -0800

    open camera. dump info.

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,70 @@
 /*
-Copyright (C) 2012-2020 tim cotter. All rights reserved.
+Copyright (C) 2012-2023 tim cotter. All rights reserved.
 */
 
 /**
-hello world example.
+drive the zwo asi astrophotography camera.
 **/
 
 #include <ASICamera2.h>
 
 #include "log.h"
 
+namespace {
+class Zwo {
+public:
+    int num_cameras_ = 0;
+    const int kCameraNumber = 0;
+    int width_ = 0;
+    int height_ = 0;
+    bool is_color_ = false;
+    int bayer_ = 0;
+
+    Zwo() = default;
+    ~Zwo() = default;
+
+    void run() {
+        LOG("Starting ZWO ASI camera.");
+
+        num_cameras_ = ASIGetNumOfConnectedCameras();
+        if (num_cameras_ != 1) {
+            LOG("Aborting.");
+            LOG("Number of cameras is "<<num_cameras_<<".");
+            LOG("Expected number is 1.");
+            return;
+        }
+
+        ASI_CAMERA_INFO camera_info;
+		ASIGetCameraProperty(&camera_info, kCameraNumber);
+		LOG("Found camera: "<<camera_info.Name);
+
+        width_ = camera_info.MaxWidth;
+        height_ = camera_info.MaxHeight;
+        LOG("Resolution: "<<width_<<" x "<<height_);
+
+        is_color_ = (camera_info.IsColorCam == ASI_TRUE);
+        if (is_color_) {
+            bayer_ = camera_info.BayerPattern;
+            LOG("Color: Bayer: "<<bayer_);
+        } else {
+            LOG("Color: Mono");
+        }
+
+		ASI_ERROR_CODE result = ASIOpenCamera(kCameraNumber);
+		if (result != ASI_SUCCESS) {
+            LOG("Failed to open camera.");
+            LOG("ASIOpenCamera("<<kCameraNumber<<"): "<<result);
+            return;
+		}
+		LOG("Opened camera.");
+
+
+    	ASICloseCamera(kCameraNumber);
+    	LOG("Closed camera.");
+        LOG("Finished.");
+    }
+};
+}
 
 int main(
     int argc, char *argv[]
@@ -19,12 +74,8 @@ int main(
 
     zwo_log::init("rawsome.log");
 
-    LOG("hello, world!");
-
-    int num_cameras = ASIGetNumOfConnectedCameras();
-    LOG("num_cameras="<<num_cameras);
-
-    LOG("goodbye, world!");
+    Zwo zwo;
+    zwo.run();
 
     return 0;
 }
