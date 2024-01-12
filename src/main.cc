06733cd3e3b435d319143e26d4fbfb4e5ef35ccf
timmer <timmerov+git@gmail.com>
2024-01-11 18:06:44 -0800

    capture continuous images in bayer. use cv to convert bayer to rgb.

--- src/main.cc
+++ src/main.cc
@@ -112,7 +112,7 @@ public:
         /** gain 100 probably means no software gain. **/
     	ASISetControlValue(kCameraNumber, ASI_GAIN, 100, ASI_FALSE);
     	/** exposure time is in microseconds. **/
-	    ASISetControlValue(kCameraNumber, ASI_EXPOSURE, 320*1000, ASI_FALSE);
+	    ASISetControlValue(kCameraNumber, ASI_EXPOSURE, 30*1000, ASI_FALSE);
 	    /** the scale seems to be 1 to 99 relative to green. defaults are 52,95. **/
     	ASISetControlValue(kCameraNumber, ASI_WB_R, 52, ASI_FALSE);
     	ASISetControlValue(kCameraNumber, ASI_WB_B, 95, ASI_FALSE);
@@ -148,25 +148,14 @@ public:
 
         /** set camera parameters **/
 
-        /** change mode and binning. **/
-        int bytes_per_pixel = 0;
-        int cv_type = 0;
-        if (0) {
-            LOG("Using Raw8.");
-            bytes_per_pixel = 1;
-            type = ASI_IMG_RAW8;
-            cv_type = CV_8UC1;
-        } else if (0) {
-            LOG("Using Raw16.");
-            bytes_per_pixel = 2;
-            type = ASI_IMG_RAW16;
-            cv_type = CV_16UC1;
-        } else if (1) {
-            LOG("Using Rgb24.");
-            bytes_per_pixel = 3;
-            type = ASI_IMG_RGB24;
-            cv_type = CV_8UC3;
-        }
+        /** change color mode. **/
+        LOG("Using Raw16.");
+        int bytes_per_pixel = 2;
+        int cv_type_bayer = CV_16UC1;
+        int cv_type_rgb = CV_16UC3;
+        type = ASI_IMG_RAW16;
+
+        /** change binning. **/
         if (1) {
             LOG("Using bin=1");
             bin = 1;
@@ -183,84 +172,90 @@ public:
         int cam_sz = wd * ht * bytes_per_pixel;
         auto cam_buffer = new std::uint8_t[cam_sz];
 
-        /** start the exposure. wait for it to finish. **/
-        auto status = ASI_EXP_WORKING;
-        ASIStartExposure(kCameraNumber, ASI_FALSE);
-        for(;;) {
-            ASIGetExpStatus(kCameraNumber, &status);
-            if (status != ASI_EXP_WORKING) {
-                break;
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        }
-        LOG("ASIGetExpStatus()="<<status);
-   		if (status == ASI_EXP_SUCCESS) {
-            result = ASIGetDataAfterExp(kCameraNumber, cam_buffer, cam_sz);
-            LOG("ASIGetDataAfterExp()="<<result);
-        }
-
-        if (type == ASI_IMG_RAW16) {
-            int minr = 65535;
-            int maxr = 0;
-            int ming = 65535;
-            int maxg = 0;
-            int minb = 65535;
-            int maxb = 0;
-            std::int64_t sumr = 0;
-            std::int64_t sumg = 0;
-            std::int64_t sumb = 0;
-            auto ptr = (std::uint16_t *) cam_buffer;
-            for (int y = 0; y < ht; y += 2) {
-                for (int x = 0; x < wd; x += 2) {
-                    int r = (unsigned int) ptr[0];
-                    int g1 = (unsigned int) ptr[1];
-                    int g2 = (unsigned int) ptr[wd];
-                    int b = (unsigned int) ptr[wd+1];
-                    minr = std::min(minr, r);
-                    maxr = std::max(maxr, r);
-                    ming = std::min(ming, g1);
-                    maxg = std::max(maxg, g1);
-                    ming = std::min(ming, g2);
-                    maxg = std::max(maxg, g2);
-                    minb = std::min(minb, b);
-                    maxb = std::max(maxb, b);
-                    sumr += r;
-                    sumg += g1 + g2;
-                    sumb += b;
-                    ptr += 2;
-                }
-            }
-            int npixels = wd * ht;
-            int avgr = sumr / npixels;
-            int avgg = sumg / (2*npixels);
-            int avgb = sumb / npixels;
-            LOG("r: "<<minr<<" "<<avgr<<" "<<maxr);
-            LOG("g: "<<ming<<" "<<avgg<<" "<<maxg);
-            LOG("b: "<<minb<<" "<<avgb<<" "<<maxb);
-        }
-
-        /** create an image buffer for the window. **/
-        cv::Size win_sz(wd, ht);
-		cv::Mat win_image(win_sz, cv_type);
-
-		/** copy the camera image to the window image. **/
-		memcpy(win_image.data, cam_buffer, cam_sz);
-		/*int row_sz = wd * bytes_per_pixel;
-		auto src = cam_buffer;
-		for (int y = 0; y < ht; ++y) {
-		    auto dst = win_image.ptr(y);
-		    std::memcpy(dst, src, row_sz);
-		    src += row_sz;
-		}*/
-
         /** create a window. **/
         cv::String win_name = "ZWO ASI";
         cv::namedWindow(win_name);
         cv::moveWindow(win_name, 50, 50);
-        cv::imshow(win_name, win_image);
 
-        /** wait for user input. **/
-        cv::waitKey(0);
+        /** create an image buffer for the window. **/
+        cv::Size win_sz(wd, ht);
+        cv::Mat win_image_bayer(win_sz, cv_type_bayer);
+        cv::Mat win_image_rgb(win_sz, cv_type_rgb);
+
+        /** show frames until use hits a key. **/
+        for(;;) {
+            /** start the exposure. wait for it to finish. **/
+            auto status = ASI_EXP_WORKING;
+            ASIStartExposure(kCameraNumber, ASI_FALSE);
+            for(;;) {
+                ASIGetExpStatus(kCameraNumber, &status);
+                if (status != ASI_EXP_WORKING) {
+                    break;
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+            //LOG("ASIGetExpStatus()="<<status);
+            if (status == ASI_EXP_SUCCESS) {
+                result = ASIGetDataAfterExp(kCameraNumber, cam_buffer, cam_sz);
+                //LOG("ASIGetDataAfterExp()="<<result);
+            }
+
+            /**if (type == ASI_IMG_RAW16) {
+                int minr = 65535;
+                int maxr = 0;
+                int ming = 65535;
+                int maxg = 0;
+                int minb = 65535;
+                int maxb = 0;
+                std::int64_t sumr = 0;
+                std::int64_t sumg = 0;
+                std::int64_t sumb = 0;
+                auto ptr = (std::uint16_t *) cam_buffer;
+                for (int y = 0; y < ht; y += 2) {
+                    for (int x = 0; x < wd; x += 2) {
+                        int r = (unsigned int) ptr[0];
+                        int g1 = (unsigned int) ptr[1];
+                        int g2 = (unsigned int) ptr[wd];
+                        int b = (unsigned int) ptr[wd+1];
+                        minr = std::min(minr, r);
+                        maxr = std::max(maxr, r);
+                        ming = std::min(ming, g1);
+                        maxg = std::max(maxg, g1);
+                        ming = std::min(ming, g2);
+                        maxg = std::max(maxg, g2);
+                        minb = std::min(minb, b);
+                        maxb = std::max(maxb, b);
+                        sumr += r;
+                        sumg += g1 + g2;
+                        sumb += b;
+                        ptr += 2;
+                    }
+                }
+                int npixels = wd * ht;
+                int avgr = sumr / npixels;
+                int avgg = sumg / (2*npixels);
+                int avgb = sumb / npixels;
+                LOG("r: "<<minr<<" "<<avgr<<" "<<maxr);
+                LOG("g: "<<ming<<" "<<avgg<<" "<<maxg);
+                LOG("b: "<<minb<<" "<<avgb<<" "<<maxb);
+            }**/
+
+            /** copy the bayer camera image to the window image. **/
+            memcpy(win_image_bayer.data, cam_buffer, cam_sz);
+
+            /** convert the bayer image to rgb. **/
+            cv::cvtColor(win_image_bayer, win_image_rgb, cv::COLOR_BayerRG2RGB);
+
+            /** show it. **/
+            cv::imshow(win_name, win_image_rgb);
+
+            /** wait for user input. **/
+            int key = cv::waitKey(30);
+            if (key >= 0) {
+                LOG("key="<<key);
+                break;
+            }
+        }
 
         /** clean up and go home. **/
         cv::destroyWindow(win_name);
