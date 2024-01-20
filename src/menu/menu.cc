4213eb605684f9b047d8c0b0f4fb72e478f74e15
timmer <timmerov+git@gmail.com>
2024-01-19 23:20:30 -0800

    add experimental menu item.

--- src/menu/menu.cc
+++ src/menu/menu.cc
@@ -6,6 +6,8 @@ Copyright (C) 2012-2024 tim cotter. All rights reserved.
 run the menu thread.
 **/
 
+#include <cmath>
+
 #include <aggiornamento/aggiornamento.h>
 #include <aggiornamento/log.h>
 #include <aggiornamento/nonblocking_input.h>
@@ -60,6 +62,11 @@ public:
             quit();
             break;
 
+        case 'x':
+        case 'X':
+            experiment();
+            break;
+
         default:
             showMenu();
             break;
@@ -70,6 +77,7 @@ public:
         LOG("Menu:");
         LOG("  F,f: toggle manual focus helper: "<<settings_->show_focus_);
         LOG("  Q,q,esc: quit ");
+        LOG("  X,x: run the experiment of the day");
     }
 
     void toggleFocus() noexcept {
@@ -83,6 +91,46 @@ public:
         /** stop all threads. **/
         LOG("MenuThread stopping all threads.");
         agm::master::setDone();
+    }
+
+    void experiment() noexcept {
+        /**
+        examine the gamma table.
+        we must do this to convert linear rgb to what's shown on the displays.
+        the idea is we're going to capture in 16 bit.
+        yeah, i know it's not really 16 bit.
+        but that's what opencv likes.
+        and we're going to stack many images into a 32 bit image.
+        the usual practice is to use a look up table.
+        but we can't make a 32 bit lookup table.
+        so we're going to squash the range of values.
+        let's say the max is 120,000.
+        we'll scale that to 539.
+        then look up the 8 bit value in the table.
+        why 539?
+        because a lookup table with 540 values is the smallest table size
+        that is a multiple of 4 and will span the entire range of 8 bit values
+        with no holes.
+        **/
+        char lut[256];
+        for (int i = 0; i < 256; ++i) {
+            lut[i] = 0;
+        }
+        double gamma = 2.2;
+        int sz = 539;
+        for (int i = 0; i <= sz; ++i) {
+            double x = std::pow(double(i)/double(sz), gamma) * 255.0;
+            int ix = std::round(x);
+            ix = std::max(0, std::min(ix, 255));
+            LOG(i<<": "<<ix);
+            lut[ix] = 1;
+        }
+        for (int i = 0; i < 256; ++i) {
+            if (lut[i] == 0) {
+                LOG("lut["<<i<<"]=0");
+            }
+        }
+        LOG("done");
     }
 };
 }
